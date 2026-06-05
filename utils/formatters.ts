de1c3616c70d3a3457af075b592a4c1bc817fc31
ivandelimaw3 <maxtopcon@gmail.com>
2026-06-05 16:09:23 -0300

    Add files via upload

---
+++ utils/formatters.ts
@@ -0,0 +1,20 @@
+
+/**
+ * Formata valores monetários para o padrão brasileiro (R$ 0,00)
+ */
+export const formatCurrency = (value: number): string => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(value);
+};
+
+/**
+ * Formata números para o padrão brasileiro (0,00)
+ */
+export const formatNumber = (value: number, decimals: number = 2): string => {
+  return new Intl.NumberFormat('pt-BR', {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals
+  }).format(value);
+};

2444a79e26b17aed8fa00245042f393c09f59be3
w3geotecnologia <w3.geotecnologia@gmail.com>
2026-08-23 15:20:11 -0300

    Add files via upload

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
-
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Header } from './Header';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from './AppSidebar';
 
@@ -33,7 +31,6 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
     setSidebarCollapsed(isCollapsiblePage);
   }, [isCollapsiblePage, location.pathname]);
 
-
   const handleMainContentHover = () => {
     if (isCollapsiblePage) {
       setSidebarCollapsed(true);
@@ -50,13 +47,11 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
       <SidebarProvider open={!sidebarCollapsed} onOpenChange={(open) => setSidebarCollapsed(!open)}>
         <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 w-full flex">
-          {/* Desktop Sidebar */}
-        <div className="hidden md:block" onMouseEnter={handleSidebarHover}>
-          <AppSidebar />
-        </div>
+          <div className="hidden md:block" onMouseEnter={handleSidebarHover}>
+            <AppSidebar />
+          </div>
 
           <div className="flex-1 flex flex-col">
-            <Header />
             <main 
               className="flex-1 p-3 sm:p-6"
               onMouseEnter={handleMainContentHover}
@@ -72,13 +67,11 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <SidebarProvider>
       <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 w-full flex">
-        {/* Desktop Sidebar */}
         <div className="hidden md:block">
           <AppSidebar />
         </div>
 
         <div className="flex-1 flex flex-col">
-          <Header />
           <main className="flex-1 p-3 sm:p-6">
             {children}
           </main>
